be6c236986775563abd17d27e49e674d76f63214
paullarasales <rkise2131@gmail.com>
2024-12-26 22:06:03 +0800

    done with the model

---
+++ model.py
@@ -0,0 +1,17 @@
+from tensorflow.keras.models import Model  # type: ignore
+from tensorflow.keras.layers import Input, LSTM, Dense # type: ignore
+
+def build_model(num_chars, latent_dim):
+    encoder_inputs = Input(shape = (None, num_chars))
+
+    encoder = LSTM(latent_dim, return_state=True)
+    _, state_h, state_c = encoder(encoder_inputs)
+    encoder_states = [state_h, state_c]
+
+    decoder_inputs = Input(shape = (None, num_chars))
+
+    decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+    decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+    
+    decoder_dense = Dense(num_chars, activation='softmax')
+    decoder_outputs = decoder_dense(decoder_outputs)
