9704db22e9bfba18ac6bddae9bb23ff496061e07
paullarasales <rkise2131@gmail.com>
2025-01-01 14:31:06 +0800

    done task 3

--- iris_train.py
+++ iris_train.py
@@ -15,3 +15,12 @@ print(data.head())
 # Task one
 mean_sepal_length = data["sepal_length"].mean()
 print(f"The mean of sepal_length {mean_sepal_length:.2f}")
+
+# Task two
+plt.figure(figsize=(8, 6))
+plt.hist(data["sepal_length"], bins=10, color='skyblue', edgecolor='black')
+plt.title('Histogram of Sepal Length')
+plt.xlabel('Sepal Length')
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.show()
